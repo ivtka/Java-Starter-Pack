8f81d443491acc4ac5924917408e5b4c57266a43
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-29 13:57:28 +0200

    For-each loop and Initialize arrays

--- src/main/java/HelloEnum.java
+++ src/main/java/HelloEnum.java
@@ -1,6 +1,8 @@
 public class HelloEnum {
     public static void main(String[] args) {
-        Fruit apple = Fruit.Apple;
-        System.out.printf("Total price of all apples: %.2f%n", apple.totalPrice());
+        Fruit[] fruits = {Fruit.Apple, Fruit.Orange};
+        for (var fruit : fruits) {
+            System.out.printf("Total price of all %s: %.2f%n", fruit.name(), fruit.totalPrice());
+        }
     }
 }
