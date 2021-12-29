e88b6498ad140a3890abde7e88d01dd247a94a3f
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-29 14:03:05 +0200

    Try Catch

--- src/main/java/CheckedException.java
+++ src/main/java/CheckedException.java
@@ -1,10 +1,15 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class CheckedException {
     public void readFile() {
         String fileName = "file does not exits";
         File file = new File(fileName);
-        FileInputStream stream = new FileInputStream(file); // throw Exception
+        try {
+            FileInputStream stream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/HelloEnum.java
+++ src/main/java/HelloEnum.java
@@ -1,8 +1,6 @@
 public class HelloEnum {
     public static void main(String[] args) {
-        Fruit[] fruits = {Fruit.Apple, Fruit.Orange};
-        for (var fruit : fruits) {
-            System.out.printf("Total price of all %s: %.2f%n", fruit.name(), fruit.totalPrice());
-        }
+        var checkedException = new CheckedException();
+        checkedException.readFile();
     }
 }
