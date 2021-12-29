3768f0bfc96e990bb9f88d6e182e90d86fd1d6fb
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-29 13:52:28 +0200

    Hello Enum

---
+++ src/main/java/HelloEnum.java
@@ -0,0 +1,6 @@
+public class HelloEnum {
+    public static void main(String[] args) {
+        Fruit apple = Fruit.Apple;
+        System.out.printf("Total price of all apples: %.2f%n", apple.totalPrice());
+    }
+}
