9fef57e9ad298fd063c8566ba4a26b6416d31265
Ivan Tkachuk <ivtkac@gmail.com>
2021-12-30 18:04:45 +0200

    Sort only odd elements

--- src/main/java/kata/Kata.java
+++ src/main/java/kata/Kata.java
@@ -1,24 +1,48 @@
 package kata;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Kata {
     public static int[] arrayDiff(int[] a, int[] b) {
         var diff = new ArrayList<Integer>();
-        for (int i = 0; i < a.length; i++) {
-            if (!contains(b, a[i])) {
-                diff.add(a[i]);
+        for (int j : a) {
+            if (!contains(b, j)) {
+                diff.add(j);
             }
         }
         return diff.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static boolean contains(int[] b, int element) {
-        for (int i = 0; i < b.length; i++) {
-            if (b[i] == element) {
+        for (int j : b) {
+            if (j == element) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static int[] sortArray(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int k : array) {
+            if (isOdd(k)) {
+                list.add(k);
+            }
+        }
+        Collections.sort(list);
+        int j = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (isOdd(array[i])) {
+                array[i] = list.get(j);
+                j++;
+            }
+        }
+        return array;
+    }
+
+    public static boolean isOdd(int element) {
+        return element % 2 != 0;
     }
 }
--- src/test/java/kata/KataTest.java
+++ src/test/java/kata/KataTest.java
@@ -14,4 +14,19 @@ public class KataTest {
         assertArrayEquals(new int[]{1, 2, 2}, Kata.arrayDiff(new int[]{1, 2, 2}, new int[]{}));
         assertArrayEquals(new int[]{}, Kata.arrayDiff(new int[]{}, new int[]{1, 2}));
     }
+
+    @Test
+    public void exampleTest1() {
+        assertArrayEquals(new int[]{ 1, 3, 2, 8, 5, 4 }, Kata.sortArray(new int[]{ 5, 3, 2, 8, 1, 4 }));
+    }
+
+    @Test
+    public void exampleTest2() {
+        assertArrayEquals(new int[]{ 1, 3, 5, 8, 0 }, Kata.sortArray(new int[]{ 5, 3, 1, 8, 0 }));
+    }
+
+    @Test
+    public void exampleTest3() {
+        assertArrayEquals(new int[]{}, Kata.sortArray(new int[]{}));
+    }
 }
