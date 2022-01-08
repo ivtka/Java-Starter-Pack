a400f593ec31f86edc6cdc174cc01d8b15438566
Ivan Tkachuk <ivtkac@gmail.com>
2022-01-08 18:56:07 +0200

    Refactor

--- src/main/java/kata/PythagoreanTriple.java
+++ src/main/java/kata/PythagoreanTriple.java
@@ -2,7 +2,6 @@ package kata;
 
 public class PythagoreanTriple {
     public int pythagoreanTriple(int[] triple){
-        return triple[2] * triple[2] == triple[0] * triple[0] + triple[1] * triple[1]
-                ? 1 : 0;
+        return triple[2] * triple[2] == triple[0] * triple[0] + triple[1] * triple[1] ? 1 : 0;
     }
 }
