4dec4365fac5626cbe129d532f67385e1197a4ec
Ivan Tkachuk <ivtkac@gmail.com>
2022-01-08 17:35:26 +0200

    Isogram

--- src/main/java/kata/Isogram.java
+++ src/main/java/kata/Isogram.java
@@ -2,8 +2,6 @@ package kata;
 
 public class Isogram {
     public static boolean  isIsogram(String str) {
-        return str.chars().map(Character::toLowerCase)
-                .filter(Character::isAlphabetic)
-                .distinct().count() == str.length();
+        return str.length() == str.toLowerCase().chars().distinct().count();
     }
 }
